5b6cc29451eeef1781e8c18df3cda70caaa14dea
Kai <kai_huang@cc-work.com>
2021-11-29 23:13:23 +0800

    建立專案

---
+++ Mao.CsProject.Rename.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mao.CsProject.Rename.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Execute();
+        }
+    }
+
+    public class App
+    {
+        /// <summary>
+        /// 主程序
+        /// </summary>
+        public void Execute()
+        {
+
+        }
+    }
+}
